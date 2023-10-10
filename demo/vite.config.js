cc4567a3180f1b876787a944601a95d8786e43b1
Adam Reis <adam@helloclub.com>
2023-10-10 21:41:19 +1300

    Fix bundling

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,10 +1,11 @@
 import {defineConfig} from 'vite'
 
 export default defineConfig({
-  publicDir: 'public',
+  publicPath: './src/public',
   root: './src',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
+    emptyOutDir: true,
+    assetsInlineLimit: 0,
   },
-  plugins: [],
 })
