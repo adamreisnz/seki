3fdafac8f4c92424834635b5499eea61282ac952
Adam Reis <adam@helloclub.com>
2023-10-18 13:54:02 +1300

    Time parsing

--- demo/src/js/demo.js
+++ demo/src/js/demo.js
@@ -80,15 +80,19 @@ export function onClick(query, handler) {
 /**
  * Time parser
  */
-export function parseTime(time) {
+export function parseTime(time = 0) {
   time = Math.floor(time)
-  if (time > 3600) {
+  if (time >= 24 * 3600) {
+    const days = String(Math.floor(time / (24 * 3600)))
+    return (days === '1') ? `1 day` : `${days} days`
+  }
+  else if (time >= 3600) {
     const hours = String(Math.floor(time / 3600)).padStart(2, '0')
     const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, '0')
     const seconds = String(time % 60).padStart(2, '0')
     return `${hours}:${minutes}:${seconds}`
   }
-  else if (time > 60) {
+  else if (time >= 60) {
     const minutes = String(Math.floor(time / 60)).padStart(2, '0')
     const seconds = String(time % 60).padStart(2, '0')
     return `${minutes}:${seconds}`
@@ -96,5 +100,37 @@ export function parseTime(time) {
   else {
     const seconds = String(time).padStart(2, '0')
     return `00:${seconds}`
+  }
+}
+
+/**
+ * Over time parser
+ */
+export function parseOverTime(overTime) {
+  if (!overTime) {
+    return ''
+  }
+  const match = overTime.match(/[0-9]+x([0-9]+)([a-z -]+)/i)
+  if (match) {
+    const time = match[1]
+    const suffix = match[2]
+    if (time >= 24 * 3600) {
+      const days = String(Math.floor(time / (24 * 3600)))
+      return (days === '1') ? `1 day ${suffix}` : `${days} days ${suffix}`
+    }
+    else if (time >= 3600) {
+      const hours = String(Math.floor(time / 3600))
+      const minutes = String(Math.floor((time % 3600) / 60))
+      const seconds = String(time % 60)
+      return `${hours}h ${minutes}m ${seconds}s ${suffix}`
+    }
+    else if (time >= 60) {
+      const minutes = String(Math.floor(time / 60))
+      const seconds = String(time % 60)
+      return `${minutes}m ${seconds}s ${suffix}`
+    }
+    else {
+      return `${time}s ${suffix}`
+    }
   }
 }
