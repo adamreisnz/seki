c8c8b9fb36922f12a6f65af5b0f5034f504034b5
Adam Reis <adam@helloclub.com>
2023-12-03 13:07:58 +1300

    Improve player generation and rendering

--- embed/src/index.js
+++ embed/src/index.js
@@ -27,93 +27,22 @@ const {
   markupTypes,
 } = constants.markup
 
+//Get helpers
+const {
+  toggleClass,
+} = helpers.util
+
 //Re-export Seki constants and helpers
 export {
   constants,
   helpers,
 }
 
-//Render button
-function renderButton(type, icon, title, onClick) {
-  const button = document.createElement('button')
-  const image = document.createElement('img')
-  image.src = icon
-  button.classList.add('seki-button')
-  button.classList.add(`seki-button-${type}`)
-  button.setAttribute('title', title)
-  button.addEventListener('click', onClick)
-  button.appendChild(image)
-  return button
-}
+//Generate captures string
+const capturesString = (count) => `${count} capture${count === 1 ? '' : 's'}`
 
-//Render navigation
-function renderControls(element, player) {
-
-  //Create buttons
-  const cFirstPos = renderButton(
-    'first', imageBackwardFast,
-    'Go to first position',
-    () => player.goToFirstPosition(),
-  )
-  const cSkipBack = renderButton(
-    'back', imageBackwardSkip,
-    'Skip backward',
-    () => player.goBackNumPositions(),
-  )
-  const cPrevPos = renderButton(
-    'previous', imageBackwardStep,
-    'Go back',
-    () => player.goToPreviousPosition(),
-  )
-  const cPlay = renderButton(
-    'play', imagePlay,
-    'Start auto play',
-    () => player.toggleAutoPlay(),
-  )
-  const cPause = renderButton(
-    'pause', imagePause,
-    'Pause auto play',
-    () => player.toggleAutoPlay(),
-  )
-  const cNextPos = renderButton(
-    'next', imageForwardStep,
-    'Go forward',
-    () => player.goToNextPosition(),
-  )
-  const cSkipForward = renderButton(
-    'forward', imageForwardSkip,
-    'Skip forward',
-    () => player.goForwardNumPositions(),
-  )
-  const cLastPos = renderButton(
-    'last', imageForwardFast,
-    'Go to last position',
-    () => player.goToLastPosition(),
-  )
-
-  //Hide pause button
-  helpers.util.toggleClass(cPause, 'seki-hidden', true)
-
-  //Append to controls
-  element.appendChild(cFirstPos)
-  element.appendChild(cSkipBack)
-  element.appendChild(cPrevPos)
-  element.appendChild(cPlay)
-  element.appendChild(cPause)
-  element.appendChild(cNextPos)
-  element.appendChild(cSkipForward)
-  element.appendChild(cLastPos)
-
-  //Listener for auto play toggling
-  player.on('autoPlayToggle', event => {
-    const {isAutoPlaying} = event.detail
-    helpers.util.toggleClass(cPlay, 'seki-hidden', isAutoPlaying)
-    helpers.util.toggleClass(cPause, 'seki-hidden', !isAutoPlaying)
-  })
-}
-
-//Helper to show all move numbers for a static board
-function showAllMoveNumbers(game, board) {
+//Show all move numbers for a static board
+const showAllMoveNumbers = (game, board) => {
   game.node
     .getAllMoveNodes()
     .forEach((node, i) => {
@@ -232,39 +161,108 @@ export function sekiPlayer(element, config = {}) {
     <div class="seki-player">
       <div class="seki-player-board-and-controls">
         <div class="seki-player-board"></div>
-        <div class="seki-controls-container"></div>
+        <div class="seki-controls">
+          <button class="seki-button seki-button-first" title="Go to first position">
+            <img src="${imageBackwardFast}" />
+          </button>
+          <button class="seki-button seki-button-back" title="Skip backward">
+            <img src="${imageBackwardSkip}" />
+          </button>
+          <button class="seki-button seki-button-previous" title="Go back">
+            <img src="${imageBackwardStep}" />
+          </button>
+          <button class="seki-button seki-button-play" title="Start auto play">
+            <img src="${imagePlay}" />
+          </button>
+          <button class="seki-button seki-button-pause seki-hidden" title="Pause auto play">
+            <img src="${imagePause}" />
+          </button>
+          <button class="seki-button seki-button-next" title="Go forward">
+            <img src="${imageForwardStep}" />
+          </button>
+          <button class="seki-button seki-button-forward" title="Skip forward">
+            <img src="${imageForwardSkip}" />
+          </button>
+          <button class="seki-button seki-button-last" title="Go to last position">
+            <img src="${imageForwardFast}" />
+          </button>
+        </div>
       </div>
       <div class="seki-info-container">
-        <div class="seki-info-blocks">
-          <div class="seki-info-block seki-info-block-half seki-info-block-black">
+        <div class="seki-info-players">
+          <div class="seki-info-block seki-info-block-black">
             <div class="seki-identity">
               <div class="seki-color seki-color-black"></div>
               <div class="seki-name-and-rank">
-                <span class="seki-name">Black</span>
-                <small></small>
+                <span class="seki-name seki-name-black">Black</span>
+                <small class="seki-small seki-rank-black"></small>
               </div>
             </div>
             <div class="seki-score">
-              <span>0 captures</span>
+              <span class="seki-captures-black">0 captures</span>
             </div>
           </div>
-          <div class="seki-info-block seki-info-block-half seki-info-block-white">
+          <div class="seki-info-block seki-info-block-white">
             <div class="seki-identity">
               <div class="seki-color seki-color-white"></div>
               <div class="seki-name-and-rank">
-                <span class="seki-name">White</span>
-                <small></small>
+                <span class="seki-name seki-name-white">White</span>
+                <small class="seki-small seki-rank-white"></small>
               </div>
             </div>
             <div class="seki-score">
-              <span>0 captures</span>
-              <span>+6.5</span>
+              <span class="seki-captures-white">0 captures</span>
+              <span class="seki-komi"></span>
             </div>
           </div>
+        </div>
+        <div class="seki-info-game-details">
+          <div class="seki-info-block">
+            <div class="seki-info-group">
+              <label class="seki-label">game</label>
+              <div class="seki-game-name"></div>
+            </div>
+            <div class="seki-info-group">
+              <label class="seki-label">date</label>
+              <div class="seki-game-date"></div>
+            </div>
+            <div class="seki-info-group">
+              <label class="seki-label">event</label>
+              <div class="seki-event"></div>
+            </div>
+            <div class="seki-info-group">
+              <label class="seki-label">result</label>
+              <a class="seki-link seki-result-toggle">show</a>
+              <div class="seki-game-result seki-hidden"></div>
+            </div>
+          </div>
+        </div>
+        <div class="seki-info-comments">
+          <div class="seki-info-block seki-comments"></div>
         </div>
       </div>
     </div>
   `
+
+  //Helper to find elements by class name within this player instance
+  const findElements = (className) => {
+    return Array.from(element.getElementsByClassName(`seki-${className}`))
+  }
+
+  //Helper to set text content of an element by class name
+  const setText = (className, text) => {
+    findElements(className).forEach(el => el.textContent = text)
+  }
+
+  //Apply click handler
+  const onClick = (className, fn) => {
+    findElements(className).forEach(el => el.addEventListener('click', fn))
+  }
+
+  //Helper to toggle an element hidden
+  const toggleHidden = (className, val) => {
+    findElements(className).forEach(el => toggleClass(el, 'seki-hidden', val))
+  }
 
   //Instantiate player and get board element
   const player = new Player(config)
@@ -274,6 +272,86 @@ export function sekiPlayer(element, config = {}) {
   //Bootstrap player onto board element
   player.bootstrap(boardElement)
 
+  //Game load handler
+  player.on('gameLoad', () => {
+    const {game} = player
+    const {black, white} = game.getPlayers()
+    const komi = game.getKomi()
+    const result = game.getGameResult()
+    const name = game.getGameName()
+    const date = game.getGameDate()
+    const eventName = game.getEventName()
+    const eventLocation = game.getEventLocation()
+    const eventRound = game.getEventRound()
+    setText('comments', '')
+    setText('name-black', black.name || 'Black')
+    setText('name-white', white.name || 'White')
+    setText('rank-black', black.rank || '')
+    setText('rank-white', white.rank || '')
+    setText('komi', komi ? `+${komi}` : '')
+    setText('game-result', result || '')
+    setText('game-name', name || '')
+    setText('game-date', date || '')
+    setText('event', '')
+    if (eventName || eventLocation || eventRound) {
+      const parts = [
+        eventName,
+        eventLocation,
+        eventRound,
+      ].filter(p => !!p)
+      if (parts.length > 0) {
+        setText('event', parts.join(', '))
+      }
+    }
+  })
+
+  //Path change handler
+  player.on('pathChange', () => {
+
+    //Get data
+    const {game} = player
+    const {black, white} = game.getCaptureCount()
+    const node = game.getCurrentNode()
+
+    //Set comments
+    if (node.hasComments()) {
+      const comments = node
+        .getComments()
+        .join('\n\n')
+      setText('comments', comments)
+    }
+    else {
+      setText('comments', '')
+    }
+
+    //Set captures
+    setText(element, 'captures-black', capturesString(black))
+    setText(element, 'captures-white', capturesString(white))
+  })
+
+  //Auto play toggle handler
+  player.on('autoPlayToggle', event => {
+    const {isAutoPlaying} = event.detail
+    toggleHidden('button-play', isAutoPlaying)
+    toggleHidden('button-pause', !isAutoPlaying)
+  })
+
+  //Bind click handlers for controls
+  onClick('button-first', () => player.goToFirstPosition())
+  onClick('button-back', () => player.goBackNumPositions())
+  onClick('button-previous', () => player.goToPreviousPosition())
+  onClick('button-play', () => player.toggleAutoPlay())
+  onClick('button-pause', () => player.toggleAutoPlay())
+  onClick('button-next', () => player.goToNextPosition())
+  onClick('button-forward', () => player.goForwardNumPositions())
+  onClick('button-last', () => player.goToLastPosition())
+
+  //Bind click handler for result toggle
+  onClick('result-toggle', () => {
+    toggleHidden('result-toggle', true)
+    toggleHidden('game-result', false)
+  })
+
   //Load game info from data attribute
   if (element.dataset.game) {
     player.load(element.dataset.game)
@@ -282,11 +360,6 @@ export function sekiPlayer(element, config = {}) {
   else {
     player.setMode(playerModes.EDIT)
   }
-
-  //Render controls
-  const controlsElement = element
-    .getElementsByClassName('seki-controls-container')[0]
-  renderControls(controlsElement, player)
 
   //Return player
   return {player}
