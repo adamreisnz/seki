d3663de865097df14119ea232be299b91867d489
Adam Reis <adam@reis.nz>
2024-01-20 11:17:27 +1300

    [fix] Result parsing

--- src/helpers/parsing.js
+++ src/helpers/parsing.js
@@ -30,8 +30,8 @@ export function parseResult(result) {
     .replace(/resign/i, 'R')
     .replace(/time/i, 'T')
     .replace(/forfeit/i, 'F')
-    .replace(/0\.03'/, 'F') //Fox uses 0.03 result for a Forfeit
-    .replace(/0\.02'/, 'T') //Fox uses 0.02 result for a Timeout
+    .replace(/0\.03/, 'F') //Fox uses 0.03 result for a Forfeit
+    .replace(/0\.02/, 'T') //Fox uses 0.02 result for a Timeout
     .replace(/¼/, '.25')
     .replace(/½/, '.5')
     .replace(/¾/, '.75')
