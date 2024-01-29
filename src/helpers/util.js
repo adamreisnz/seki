b22cc6234c6859f81f311c0ba0a93d5d122e1cd9
Adam Reis <adam@reis.nz>
2024-01-29 13:43:31 +1300

    Fix date string generation

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -201,8 +201,8 @@ export function throttle(fn, interval) {
  * Date string generator
  */
 export function dateString(date = new Date()) {
-  const day = date.getDate()
-  const month = date.getMonth() + 1
-  const year = date.getFullYear()
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = String(date.getFullYear())
   return `${year}-${month}-${day}`
 }
