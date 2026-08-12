6f2dc21b71dc1efa8a401e1f64dfcacd584203e4
Adam Reis <adam@reis.nz>
2026-08-13 09:15:27 +1200

    4.1.1

--- src/constants/app.js
+++ src/constants/app.js
@@ -3,7 +3,7 @@
 
 //App details
 export const appIdentifier = '@reis/seki' //pkg.name
-export const appVersion = '4.1.0' //pkg.version
+export const appVersion = '4.1.1' //pkg.version
 export const appName = 'Seki'
 
 //Supported kifu formats
